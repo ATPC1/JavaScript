647edec5a8d59c1afd0af04d2d69ac427bb268e6
anurag_tiwari_001 <anuragtiwari0132@gmail.com>
2026-06-24 20:18:19 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer Version
+
+//alert(2+2)// we r using node no browser
+console.log(3+3) //code readability should be high
+
+console.log("Anurag")
+
+let name = "Anurag"
+let age = 21
+let isLoggedIn = true
+
+/*
+1.Premitive DataType
+
+number = 2 to power 53
+bigint(for big numerals)
+string = ""
+boolean = true/false
+null = Special type(standalone value)
+undefined = not defne
+symbol = unique
+
+2. Onject
+*/
+console.log(typeof "Anurag");
