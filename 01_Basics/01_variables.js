c9467954fcfbee41378828746719fdd465776fa0
anurag_tiwari_001 <anuragtiwari0132@gmail.com>
2026-06-24 13:02:05 +0000

    leanring about variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accID = 123456 
+let accEmail = "anurag@gmail.com"
+var accPassword = "1234"
+accCity = "Mumbai"
+let accState;
+
+accEmail = "anurag@yahoo.com"
+accPassword = "12345"
+accCity = "Bangalore"
+
+console.log(accID);
+console.table([accID, accEmail, accPassword, accState, accCity])
