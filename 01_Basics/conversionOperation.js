f9c58054e12ee3fd9fc1a134b6c182795281627f
anurag_tiwari_001 <anuragtiwari0132@gmail.com>
2026-06-25 09:44:34 +0000

    conversion operation

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,51 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+/*
+"33" = 33
+33abs = NaN
+true = 1; False = 0;
+*/
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(isLoggedIn)
+
+ let soneNumber = 11
+ let numString = String(soneNumber)
+ console.log(typeof soneNumber);
+ console.log(numString);
+ 
+ console.log(soneNumber);
+ console.log(typeof numString);
+  
+ let str1 = "Anurag"
+ let str2 = "mohan"
+ let str3 = str1 + str2;
+ console.log(str3);
+
+ console.log(1 + "2")
+ console.log("1"+2+2);
+ console.log(1+2+"2");
+ /*
+ 1
+number
+11
+11
+string
+Anuragmohan
+12
+122
+32
+ */
+let val = 100
+++val;//counter     
+console.log(val);
+
+  
